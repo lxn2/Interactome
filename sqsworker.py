a5d3b42c32b5d4cc375f09b3fb44c22482e81add
Nathan Lucyk <nlucyk@cellnetix.com>
2014-01-26 23:31:28 -0800

    Added SNS error message and documentation

--- sqsworker.py
+++ sqsworker.py
@@ -1,91 +1,90 @@
 '''
-@Author: Nathan Lucyk
-@Date: 1.26.2014
+	@Author: Nathan Lucyk
+	@Date: 1.26.2014
 
+	This program polls our AWS SQS for messages, processes them, and deletes them from the SQS.
+
+	A two threaded service was chosen for a variety reasons:
+		In python, only one thread can execute python code at a time.
+		No benefit of multiple processing threads (ASyncMessage) was found during testing (multiple threads performed marginally worse)
+		The worker thread (ASyncMessage) was left open due to the anticipated behavior of messages trickling in, not having bursts of messages. 
+
+	This program uses SNS to fanout an alert of crashes.
 '''
 import threading
 import boto.sqs
+import boto.sns
 import time
-import sys
 import logging
-from Queue import Queue
+import Queue
 from boto.sqs.message import Message
 
+logging.basicConfig(filename='sqsworker.log', level=logging.INFO, format='%(asctime)s -- %(levelname)s: %(message)s')
+
+'''
+	A synchronous way of handling messages from the queue passed in.
+'''
 class ASyncMessage(threading.Thread):
 	def __init__(self, queue, deletionQ, deletionConnection):
+		'''
+			queue: the queue that stores the messages
+			deletionQ: the SQS queue to delete the message from (This should be the same SQS that the message in the other queue came from)
+			deletionConnection: connection to SQS
+				Note: both deletionQ and connection should be unique per thread
+		'''
 		threading.Thread.__init__(self)
 		self.queue = queue
 		self.delQ = deletionQ
 		self.deletionConnection = deletionConnection
-	#Process the messages from the queue
+	# Process the messages from the queue
 	def run(self):
 		while(True):
 			try:
 				msg = self.queue.get(True) #blocks if queue is empty
+				# Msg processing goes here
 				self.deletionConnection.delete_message(self.delQ, msg)
 			except Exception, e:
 				logging.error(e)
 			
 
-
-#connection used for message deletion
-deletingConection = boto.sqs.connect_to_region("us-west-2", aws_access_key_id='AKIAJULVYOWLDWYEL45Q', aws_secret_access_key='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')
-delSQSQueue = deletingConection.get_queue("Interactome-1")
-#Shared, thread-safe, queue for the messages polled
-messageQueue = Queue()
-
 def main():
-	conn = boto.sqs.connect_to_region("us-west-2", aws_access_key_id='AKIAJULVYOWLDWYEL45Q', aws_secret_access_key='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')
-	print ("connected")
-	sqsQueue = conn.get_queue("Interactome-1")
+	QUEUE_NAME = "Interactome-1"
+	REGION = "us-west-2"
+	ACCESS_KEY = 'AKIAJULVYOWLDWYEL45Q'
+	SECRET_KEY = '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'
+	# Max messages is used to casue the Queue object to throw a full exception.
+	# The value will probably need to be tweaked depending on the systems using sqsworker.
+	MAX_MESSAGES = 50
+	messageQueue = Queue.Queue(MAX_MESSAGES)
 
-	if (sqsQueue== None):
-		print("failed to connect")
+	# Setup polling connection
+	conn = boto.sqs.connect_to_region(REGION, aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+	sqsQueue = conn.get_queue(QUEUE_NAME)
+	if (sqsQueue == None):
+		logging.error("failed to connect to polling queue")
 		return
+	logging.info("Polling connection made.")
 
-	
-	count = getTotal = readsMade = peak = 0
-	start = time.clock()
+	# It's important that every thread has its own connection
+	deletingConection = boto.sqs.connect_to_region(REGION, aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+	delSQSQueue = deletingConection.get_queue(QUEUE_NAME)
 	messageProcThread = ASyncMessage(messageQueue, delSQSQueue, deletingConection)
-	messageProcThread.setDaemon(True) #Daemons die if mainthread ends
+	messageProcThread.setDaemon(True) #Daemons die abruptly if mainthread ends
 	messageProcThread.start()
-	while (count < 250):
+	
+	while (True):
 		try:
-			getStart = time.clock()
-			rs = sqsQueue.get_messages(10)
-			getTotal += (time.clock() - getStart)
-			readsMade += 1
-			messagesGot = len(rs)
-			if(peak < messagesGot):
-				peak = messagesGot
-			count += messagesGot
-			for message in rs:
+			receivedMsgs = sqsQueue.get_messages(10)
+			for message in receivedMsgs:
 				messageQueue.put(message)
 		except Exception, e:
-			print("ERROR: ", e)
-			break
-	elapsed = (time.clock() - start)
-	print("the time it took to end: ", (elapsed - getTotal) )
-	print("readsMade: ", readsMade, " peak: ", peak)
-	print(getTotal)
+			logging.error(e)
+			# Sends a msg to SNS to inform everyone subscribed about the crash
+			snsErr = boto.sns.connect_to_region(REGION, aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+			snsErr.publish("arn:aws:sns:us-west-2:005837367462:error_report", "sqsworker.py has crashed. See logs.")
+			return
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-	# Use this to place a large amount of messages
-	# for i in range(1,1000):
-	# 	msgName = 'mass example' + str(i)
-	# 	m = Message()
-	# 	m.set_body(msgName)
-	# 	try:
-	# 		status = q.write(m)
-	# 	except Exception, e:
-	# 		print("e: ", e)
-	# 	else:
-	# 		if(status == False):
-	# 			print("status failed")
+    logging.info("sqsworker stopped.")
