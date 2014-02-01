682215543724b0f1944c8d81064a195ef7f9d26f
henry <yogmanh@seattleu.edu>
2014-01-31 17:59:01 -0800

    base play with integrating google iam login in angular directive

--- interactomeWeb/app/scripts/app.js
+++ interactomeWeb/app/scripts/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('interactomeApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-  //'bootstrap.js'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+    //'bootstrap.js'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
