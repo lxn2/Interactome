23a948c8abdb4a530bb4edfbaf6a09dacda39ad2
henry <yogmanh@seattleu.edu>
2014-02-07 17:05:52 -0800

    adding comments and cleaning up stuff

--- interactomeWeb/app/scripts/app.js
+++ interactomeWeb/app/scripts/app.js
@@ -3,21 +3,31 @@ angular.module('interactomeApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
+    'interactomeApp.Awsservice',
+    'interactomeApp.Userservice',
     'ngRoute'
     //'bootstrap.js'
 ])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
 
-// in scripts/app.js
+.config(function(AwsserviceProvider) {
+    AwsserviceProvider
+        .setArn(
+            'arn:aws:iam::005837367462:role/newRole');
+})
+
+
+.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+
 window.onLoadCallback = function() {
     // When the document is ready
     angular.element(document).ready(function() {
--- interactomeWeb/app/scripts/controllers/main.js
+++ interactomeWeb/app/scripts/controllers/main.js
@@ -3,6 +3,7 @@
 angular.module('interactomeApp')
     .controller('MainCtrl', function($scope) {
 
+        // This function sets the user authentication from googleSignin directive 
         $scope.signedIn = function(oauth) {
             UserService.setCurrentUser(oauth)
                 .then(function(user) {
