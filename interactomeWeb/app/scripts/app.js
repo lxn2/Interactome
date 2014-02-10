3b2c0643aaff3beded6d0a5029cb34d68474c838
henry <yogmanh@seattleu.edu>
2014-02-10 00:36:20 -0800

    fixed another conflict but google+ is being weird again

--- interactomeWeb/app/scripts/app.js
+++ interactomeWeb/app/scripts/app.js
@@ -29,7 +29,7 @@ angular.module('interactomeApp', [
         });
 });
 
-/*
+
 
 window.onLoadCallback = function() {
     // When the document is ready
@@ -41,4 +41,3 @@ window.onLoadCallback = function() {
         });
     });
 }
-*/
--- interactomeWeb/app/scripts/controllers/main.js
+++ interactomeWeb/app/scripts/controllers/main.js
@@ -1,16 +1,12 @@
 'use strict';
 
 angular.module('interactomeApp')
-<<<<<<< HEAD
-    .controller('MainCtrl', function($scope, Awsservice, Userservice) {
-=======
     .controller('MainCtrl', function($scope, UserService) {
->>>>>>> 016c8e5d4487484217ccdf80674846c1d0b84f14
 
         // This function sets the user authentication from googleSignin directive. 
         $scope.signedIn = function(oauth) {
             // Google authentication passed into userService to hold onto and track user.
-            Userservice.setCurrentUser(oauth)
+            UserService.setCurrentUser(oauth)
                 .then(function(user) {
                     $scope.user = user;
                 });
