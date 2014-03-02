a8d3c8c401f93ef9c28fe560c69e7710775bf73a
Nathan Lucyk <nlucyk@cellnetix.com>
2014-03-01 18:33:08 -0800

    DynamoRefactor: changed rec service to use dynamo id

--- interactomeWeb/app/scripts/services/recommendationservice.js
+++ interactomeWeb/app/scripts/services/recommendationservice.js
@@ -9,32 +9,26 @@ angular.module('interactomeApp.RecommendationService', [])
   .factory('RecommendationService', function RecommendationService($q) {
     var service = {
       // getRecs:
-      //   @abstractList: should be a list of the full url. Relies on the assumption of a '/' and .json to work.
-      //   Returns: a promise which will resolve to an array of S3 links to abstracts that may or may not exist.
+      //   @abstractList: should be a list of the dynamo Id's
+      //   Returns: a promise which will resolve to an array of hashes that have paper data from dynamo.
       getRecs: function(abstractList) {
           var defered = $q.defer();
 
           var limit = 10 + abstractList.length; // min of abstracts needed to make sure no duplicates returned
-          var sourceAbstracts = [];
-
-          // This will get the last index of / and grab the substring until the end of it.
-          // This will create an array of just the Abstract####.json part of the url.
-          for(var i = 0; i < abstractList.length; i++)
-            sourceAbstracts.push(abstractList[i].substr(abstractList[i].lastIndexOf("/")+1));
 
           // Scan table for limit number of papers
-          if(sourceAbstracts.length > 0) {
+          if(abstractList.length > 0) {
             var paperTable = new AWS.DynamoDB({params: {TableName: "Paper"}});
             var returnedPapers  = [];
             paperTable.scan({Limit: limit}, function(err, data) {
               if(err)
                 console.log(err);
               else { 
+                var paperId = "";
                 for(var i = 0; i < limit; i++) {
-                  var fullLink = data.Items[i].Link.S;
-                  var endOfLink = fullLink.substr(fullLink.lastIndexOf("/")+1);
-                  if (sourceAbstracts.indexOf(endOfLink) == -1 )// not in list sent in
-                    returnedPapers.push({Id:data.Items[i].Id.S, Link: data.Items[i].Link.S})
+                  paperId = data.Items[i].Id.S;
+                  if (abstractList.indexOf(paperId) == -1 )// not in list sent in
+                    returnedPapers.push({Id: paperId, Link: data.Items[i].Link.S})
                 }
                 defered.resolve(returnedPapers);
               }
