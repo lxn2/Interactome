c0ed561055c51960696c73b2e3afff849dc57e6d
Ly <lxn2@uw.edu>
2014-03-10 14:03:03 -0700

    switched topicpanelitem template code out to style maxheight of each topic DOM

--- interactomeWeb/app/scripts/directives/topicpanelitem.js
+++ interactomeWeb/app/scripts/directives/topicpanelitem.js
@@ -14,16 +14,31 @@ angular.module('interactomeApp')
 		    controller: ['$scope', 'AwsService', function($scope, AwsService) {          
           $scope.scopePapersList = [];
     	}],
-    	template: '<accordion>' +
-                  '<accordion-group heading="{{topicName}}">' +
+    	template: '<div class="accordion-group" class="limSize">' + 
+                  '<div class="accordion-heading" ><a class="accordion-toggle" ng-click="isOpen = !isOpen">{{topicName}}</a></div>' +
+                  '<div class="accordion-body" collapse="!isOpen" ng-class="{someClass:isOpen}">' +
+                    '<div class="accordion-inner">' +
                       '<li ng-repeat="paper in scopePapersList track by $index">' + // track by $index solves ng-repeat duplicate error: http://stackoverflow.com/questions/16296670/angular-ng-repeat-error-duplicates-in-a-repeater-are-not-allowed
                         '{{paper}}' +
                       '</li>' +
-                  '</accordion-group>' +
-                '</accordian>',
+                    '</div>' +
+                  '</div>' +
+                '</div>'
+      ,
       link: function (scope, element, attrs) {
         scope.topicName = attrs.topicName;
         scope.scopePapersList = ((attrs.papersList).replace(/['"\[\]]/gi,'')).split(','); // removes quotations and brackets, converts string into array
       }
     };
   });
+
+
+/* 
+'<accordion>' +
+                  '<accordion-group heading="{{topicName}}">' +
+                      '<li ng-repeat="paper in scopePapersList track by $index">' + // track by $index solves ng-repeat duplicate error: http://stackoverflow.com/questions/16296670/angular-ng-repeat-error-duplicates-in-a-repeater-are-not-allowed
+                        '{{paper}}' +
+                      '</li>' +
+                  '</accordion-group>' +
+                '</accordian>'
+                */
