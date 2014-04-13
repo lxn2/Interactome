9c193b54e1c03f13f3dd1785b43582ca93b660b1
Nathan Lucyk <nlucyk@cellnetix.com>
2014-04-13 02:14:26 -0700

    dataLoader: cleaned up code and added documentation Removed index text files, logging will now be good enough for everything.

--- dataLoader.py
+++ dataLoader.py
@@ -1,24 +1,23 @@
 '''
-    @Author: Ly Nguyen & Nathan Lucyk
-    @Date: 2.4.2014
+This script is used to upload the abstracts to s3, and users and abstract links to dynamodb.
+    Note: Some unicode characters will come out escaped or encoded. E.G., dash (u'2013') comes out as its utf-8 hex code and ≥ will be escaped as &#8805;
 
-    This script is used to upload the abstracts in json format to s3, and users and abstract links to dynamodb. Currently this script does not work with non-ASCII encodings.
-    However, certain encodings like utf-8 and unicode may still work.
+This script will most likely require changing for using with different spreadsheets of data. 
+The WRITES_ALLOWED_PER_LOOP constant in the functions will need to be changed based on dynamodb tables' throughputs.
+
+Requirements for this script:
+
+The abstracts excel file MUST have these as its columns (in order, but spelling doesn't matter)
+ControlNumber  PRESENTATIONNUMBER  ABSTRACT TITLE  Abstract    SUBCLASS    CATEGORY-SUBCAT-SUBCLASS    KEYWORD1    KEYWORD2    KEYWORD3    KEYWORD4    SESSIONTITLE    PRESENTERFIRSTNAME  PRESENTERLASTNAME   PRESENTERINSTITUTIONNAME    PRESENTERCONTACTEMAIL
+
+The users excel file MUST have these as its columns (in order, but spelling doesn't matter)
+ACTIVITYNAME    PROGRAMSTATUSNAME   CONTROLNUMBER   PRESENTATIONNUMBER  AUTHORFIRSTNAME AUTHORLASTNAME  INSTITUTIONNAME CONTACTEMAIL
+
+Abstracts and Users excel files must correspond to eachother and not to data already in the database. 
+I.E., a newly uploaded user will not be attached to an abstract if it was uploaded prior to running the script.
 
 
-    This script is unreliable and should be refactored and improved. The script does not work for non-english words (no accents allowed!), 
-    and makes some assumptions about the data which may not be true such as the presentation number being an integer or that it exists at all.
-    To make this fault more tolerable, the index of whatever failed is printed to a .txt file and logged in a log file. 
-
-    Timings were done while uploading large amounts of users to dynamo and abstracts to s3 and found that a large majority of the work is the upload
-    process.
-
-    Future extensions:
-        Get multiple encodings to work to allow more data to be added. 
-        Get rid of globals. 
-        Having a whitelist of attributes we do accept into dynamo / s3. 
-        Way to automagically convert fieldnames into acceptable (uniform from whitelist) formats (E.G., CONTACTEMAIL -> email)
-
+    Note: this has only been tested on xlsx files, but will most likely work with any excel extention.
 '''
 
 import sys
@@ -34,8 +33,8 @@ from boto import dynamodb2
 from openpyxl import load_workbook
 
 SEQUENCER_TABLE_NAME = 'Sequencer'
-USER_TABLE_NAME = 'User_Test'
-PAPER_TABLE_NAME = 'Paper_Test'
+USER_TABLE_NAME = 'User'
+PAPER_TABLE_NAME = 'Paper'
 ABSTRACT_BUCKET_NAME = 'sagebionetworks-interactome-abstracts'
 PRESENTATION_NUM_EXCEL_FIELD = 'PresentationNumber'
 LASTNAME_EXCEL_FIELD = 'LastName'
@@ -60,7 +59,7 @@ def connectToAWS(authCSVFileName):
     global usersTable
     global papersTable
     global abstractToPaperDict
-    # Practically every line could throw an exception so we use one try
+    
     try:
     # Opens the credentials and then uses the first line. Using DictReader may be overkill
         csvfile = open(authCSVFileName, 'rU') 
@@ -71,7 +70,7 @@ def connectToAWS(authCSVFileName):
         # Get S3 and dynamo connection
         s3Conn = s3.connect_to_region(REGION, aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
         dynamoConn = dynamodb2.connect_to_region(REGION, aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
-        # Use connections to load tables
+        
         sequencerTable = Table(SEQUENCER_TABLE_NAME, connection=dynamoConn)
         usersTable = Table(USER_TABLE_NAME, connection=dynamoConn)
         papersTable = Table(PAPER_TABLE_NAME, connection=dynamoConn)
@@ -100,17 +99,17 @@ def getSequence():
     return sequenceNumber
 
 '''
-    Parses the csv file and adds the users found within to the users table. 
-        Warning: This does not check for dupe users.
+    Parses the excel file and adds the users found within to the users table with their connection to an abstract.
+    This will edit the papersTable to add an entry into their Authors attribute.
+    Will check for duplicate users based off of email address. 
 '''
 def addUsers(excelFileName):
+    # 1 write per loop for both the user and papers tables of dynamodb
     WRITES_ALLOWED_PER_LOOP = 32
     # Load the excel stuff. See http://pythonhosted.org/openpyxl/api.html#module-openpyxl-reader-iter-worksheet-optimized-reader
     workbook = load_workbook(filename = excelFileName, use_iterators = True)
     first_sheet = workbook.get_sheet_names()[0]
     worksheet = workbook.get_sheet_by_name(first_sheet)
-
-    lostIndexFile = open("indexesNotAddedUSERS.txt", 'w')
 
     rowIndex = 0
 
@@ -134,7 +133,7 @@ def addUsers(excelFileName):
 
             # query to see if user was already added
             userId = ''
-            userQueryResults = usersTable.query(Email__eq=email, index='Email-index') # READ 1-3 USERS
+            userQueryResults = usersTable.query(Email__eq=email, index='Email-index')
             listUserQueryResults = list(userQueryResults)
             # We check all results due to moving institutions, changing names, and such.
             for user in listUserQueryResults:
@@ -143,11 +142,10 @@ def addUsers(excelFileName):
                     break
             dynamoPaperId = abstractToPaperDict[hashKey] if (hashKey in abstractToPaperDict) else ""
             try:
+                # If new user (email not in dynamo already)
                 if(userId == ''):
-
                     sequenceNumber = getSequence()
                     if (sequenceNumber == None):
-                        lostIndexFile.write(str(rowIndex) + "\n")
                         logging.error("Unabled to sequence item. Index not added: " + str(rowIndex))
                         continue
                     userId = "User" + sequenceNumber
@@ -160,45 +158,46 @@ def addUsers(excelFileName):
                     }
                     if(dynamoPaperId != ""):
                         newItem['Papers'] = set([dynamoPaperId])
-                    usersTable.put_item(data=newItem) # WRITE USER
+                    usersTable.put_item(data=newItem)
+
+                # Else it's an old user and has a paper to add to them
                 elif(hashKey in abstractToPaperDict):
-                    # update user to have paperId
-                    userItem = usersTable.get_item(Id=userId) # READ User
+                    userItem = usersTable.get_item(Id=userId)
                     if('Papers' in userItem):
                         userItem['Papers'].add(dynamoPaperId)
                     else:
                         userItem['Papers'] = set([dynamoPaperId])
-                    userItem.save() # WRITE USER
+                    userItem.save()
+
+                # Else, trying to add a user already in dynamo without a paper to add to them
                 else:
-                    lostIndexFile.write(str(rowIndex) + "\n")
                     logging.error("Unabled to find hashkey in dict. Index not added: " + str(rowIndex))
                     continue
                 # Update paper to have author
                 if(dynamoPaperId != ""):
                     print "dynamopaperid isnt empty" + str(rowIndex)
-                    paperItem = papersTable.get_item(Id=dynamoPaperId) # READ PAPER
+                    paperItem = papersTable.get_item(Id=dynamoPaperId)
                     if('Authors' in paperItem):
                         paperItem['Authors'].add(userId)
                     else:
                         paperItem['Authors'] = set([userId])
-                    paperItem.save() # WRITE PAPER
+                    paperItem.save()
                 else:
                     logging.error("Unable to find dynamopaperid. Index not added: " + str(rowIndex))
             except Exception, e:
-                lostIndexFile.write(str(rowIndex) + "\n")
                 logging.error("Unable to add to usersTable. Index not added: " + str(rowIndex) + "\nError: " + str(e))
         except Exception, e:
-            lostIndexFile.write(str(rowIndex) + "\n")
             logging.error("Unknown abstract error. Index not added: " + str(rowIndex) + "\nErr: " + str(e))
 
-    lostIndexFile.close()
+
 
 '''
-    Parses the csv file for abstracts. It will query to find if the author is already a user. If so, puts the user id with the abstract
-    The abstract will be dumped into S3 in json format. The URL will be stored in the papers table.
+    Parses the excel file for abstracts. The abstract and its metadata will be dumped into S3. The URL will be stored in the papers table.
+    The control and presentation numbers are used as a key to a dict for use in addUsers function.
         Warning: does not check for dupes
 '''
 def addAbstracts(excelFileName):
+    # Write throughput for dynamodb table, one write per loop
     WRITES_ALLOWED_PER_LOOP = 32
 
     # Load the excel stuff. See http://pythonhosted.org/openpyxl/api.html#module-openpyxl-reader-iter-worksheet-optimized-reader
@@ -206,7 +205,6 @@ def addAbstracts(excelFileName):
     first_sheet = workbook.get_sheet_names()[0]
     worksheet = workbook.get_sheet_by_name(first_sheet)
 
-    lostIndexFile = open("indexesNotAddedABSTRACTS.txt", 'w')
     abstractsBucket = s3Conn.get_bucket(ABSTRACT_BUCKET_NAME)
     abstractKey = Key(abstractsBucket)
     rowIndex = 0
@@ -232,6 +230,9 @@ def addAbstracts(excelFileName):
                 abstractTitle = row[2].internal_value.encode('utf-8')
 
                 # If the cell is empty we replace it with a blank string.
+                # The abstract and its title are not checked because we do not want 
+                #   anything that doesn't have both of those.
+                # Presenter stuff probably doesn't need to be encoded but is anyway.
                 subclass = "" if(row[4].internal_value is None) else row[4].internal_value.encode('utf-8')
                 catSubcatSubclass = "" if(row[5].internal_value is None) else row[5].internal_value.encode('utf-8')
                 keyword1 = "" if (row[6].internal_value is None) else row[6].internal_value.encode('utf-8')
@@ -258,39 +259,33 @@ def addAbstracts(excelFileName):
                     "PresenterInstitution": presenterInstitution,
                     "PresenterEmail": presenterEmail }
             except:
-                 lostIndexFile.write(str(rowIndex) + "\n")
                  logging.error("S3Formatting failed. Index not added: " + str(rowIndex))
                  continue
 
             sequenceNumber = getSequence()
             if (sequenceNumber == None):
-                lostIndexFile.write(str(rowIndex) + "\n")
                 logging.error("Unabled to sequence item. Index not added: " + str(rowIndex))
                 continue
 
-            abstractKey.key = str(rowIndex % 32) + 'Abstract' + '.json'
+            abstractKey.key = 'Abstract' + sequenceNumber +'.json'
             abstractKey.set_metadata("Content-Type", 'application/json')
             abstractKey.set_contents_from_string(s3Format)
             abstractKey.make_public()
             abstractUrlLink = abstractKey.generate_url(0, query_auth=False, force_http=True)
 
-            # Now link the S3 url to an entry in the dynamodb table
-            dynamoPaperId = 'PaperTEST'+ sequenceNumber
+            # Now link the S3 url and abstract title to an entry in the dynamodb table
+            dynamoPaperId = 'Paper'+ sequenceNumber
             newItem = {'Id': dynamoPaperId, 'Link': abstractUrlLink, 'Title': abstractTitle}
             try:
                 papersTable.put_item(data=newItem)
             except:
-                lostIndexFile.write(str(rowIndex) + "\n")
                 logging.error("Unabled to add to papers table. Index not added: " + str(rowIndex))
             else:
+                # This is how we link Authors to their papers and vice versa.
                 abstractToPaperDict[hashKey] = dynamoPaperId
 
         except Exception, e:
-            lostIndexFile.write(str(rowIndex) + "\n")
             logging.error("Unknown abstract error. Index not added: " + str(rowIndex) + "\nErr: " + str(e))
-    # print "abstractsToPapers! ------------------- \n"
-    # print abstractToPaperDict
-    lostIndexFile.close()
 
 
 def main(argv=sys.argv):
@@ -303,12 +298,11 @@ def main(argv=sys.argv):
     parser.add_argument('--abstracts', dest='abstractsPath', help='path to abstracts csv file', default=argparse.SUPPRESS, required=True)
     options = parser.parse_args(argv[1:])
 
-    connectToAWS(options.usersAuth)
+    connectToAWS(options.usersAuth) # Sets up all resources
 
-    if hasattr(options, 'usersPath') and hasattr(options, 'abstractsPath'):
-        addAbstracts(options.abstractsPath)
-        addUsers(options.usersPath)
+    # Abstracts MUST come before users.
+    addAbstracts(options.abstractsPath)
+    addUsers(options.usersPath)
         
-
 if __name__ == "__main__":
     sys.exit(main())
